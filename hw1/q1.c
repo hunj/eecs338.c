95aa0d93b5ae226f0c33488b6fd3b29771abb696
Hun Jae Lee <hunjael@gmail.com>
2016-09-11 23:06:59 -0400

    initial commit

---
+++ hw1/q1.c
@@ -0,0 +1,34 @@
+/**
+	Hun Jae Lee (hxl224)
+	EECS338: Homework #2 Question 1
+	Due 160914 Wed
+
+	- Description:
+	Creates one parent with two childrens, and display the IDs in the order in which the processes are created.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	pid_t pid1, pid2;
+
+	printf("Process 0: %d\tParent: %d\n", getpid(), getppid());
+	/* fork a child process */
+	pid1 = fork();	
+	pid2 = fork();
+	
+	// Assignment does not require fork error check (pid < 0)
+	if (pid1 == 0 && pid2 != 0) { 
+		printf("Process 2: %d\tParent: %d\n", getpid(), getppid());
+	}
+	else if (pid1 != 0 && pid2 == 0) {
+		printf("Process 1: %d\tParent: %d\n", getpid(), getppid());
+	}
+	else {
+		wait(NULL);
+	}
+    return 0;
+}
